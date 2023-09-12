60fe2e6215a1ba60e7a1fb07160f6c91f6e2d7d4
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-09-12 23:03:59 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,16 @@
+import { Component } from 'react';
 import { GlobalStyle } from './GlobalStyle';
 import { Searchbar } from './Searchbar/Searchbar';
 
-export const App = () => {
-  return (
-    <div>
-      <GlobalStyle />
-      <Searchbar />
-    </div>
-  );
-};
+export class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <GlobalStyle />
+        <Searchbar />
+      </div>
+    );
+  }
+}
