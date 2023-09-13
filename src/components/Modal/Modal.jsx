e22c49c1e492edca5e5a37f04ff233cce7cd0836
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-09-13 17:24:25 +0300

    update

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -6,18 +6,15 @@ const modalRoot = document.querySelector('#modal-root');
 
 export class Modal extends Component {
   componentDidMount() {
-    console.log('did mount');
     window.addEventListener('keydown', this.handleKeyDown);
   }
 
   componentWillUnmount() {
-    console.log('will umount');
     window.removeEventListener('keydown', this.handleKeyDown);
   }
 
   handleKeyDown = e => {
     if (e.code === 'Escape') {
-      console.log('esc');
       this.props.onClose();
     }
   };
