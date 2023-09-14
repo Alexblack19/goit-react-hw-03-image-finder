8a0d818497fc443e573b2df91f084b61ad18a01c
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-09-14 13:26:09 +0300

    update

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -2,7 +2,12 @@ import { Component } from 'react';
 import { Header, Form, Button, Span, Input } from './Searchbar.styled';
 
 export class Searchbar extends Component {
-  state = {};
+  state = { photoTitle: '' };
+
+  handleChange = e => {
+    this.setState({photoTitle: e.currentTarget.value})
+  };
+
   render() {
     return (
       <Header>
@@ -16,6 +21,7 @@ export class Searchbar extends Component {
             autoComplete="off"
             autoFocus
             placeholder="Search images and photos"
+            onChange={this.handleChange}
           />
         </Form>
       </Header>
