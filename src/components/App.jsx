bd2045767d4962db01b08b22b7a0eeafaa25c51d
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-09-14 10:51:59 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,7 +24,7 @@ export class App extends Component {
         {showModal && (
           <Modal onClose={this.toggleModal}>
             <img
-              src="https://pixabay.com/get/g8058c8e35d084e009c5fce21453557778c067c7228724816f4580a502e6668a0035c9a72e587a399e74197fa661c374db16ad61cd2094ca423fddfb48375fca0_1280.jpg"
+              src="https://pixabay.com/get/g000f464bc8125bcf88c7e9a03c1ac1732d9d8a1be8e5192a91618ddc2b4bdd4ab1a7797e2179cae4abcfa9808e2634e5cb8c3292874177ad012941abee2cd5da_1280.jpg"
               alt=""
             />
             <button type="button" onClick={this.toggleModal}>
