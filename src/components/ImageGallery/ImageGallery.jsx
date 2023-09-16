69fe02ba0fd82ad4b9d9c2eadbceb273b45edd98
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-09-16 14:42:44 +0300

    update

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,3 +1,4 @@
+import Notiflix from 'notiflix';
 import { Component } from 'react';
 import { fetchPhoto } from '../../api/image-api';
 import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem';
@@ -6,21 +7,23 @@ import { Gallery } from './ImageGallery.styled';
 export class ImageGallery extends Component {
   state = {
     dataPhoto: null,
+    error: '',
   };
 
   componentDidUpdate(prevProps, prevState) {
     const searchTag = this.props.photoTag;
     if (prevProps.photoTag !== searchTag) {
-      fetchPhoto(searchTag, 2)
+      fetchPhoto(searchTag, 3)
         .then(data => this.setState({ dataPhoto: data.hits }))
-        .catch(error => console.log(error));
+        .catch(error => this.setState({ error: error.message }));
     }
   }
 
   render() {
-    const { dataPhoto } = this.state;
+    const { dataPhoto, error } = this.state;
     return (
       <Gallery>
+        {error && Notiflix.Notify.failure(error, { position: 'center-center', fontSize: '16px', width:'320px' })}
         {dataPhoto &&
           dataPhoto.map(item => (
             <ImageGalleryItem key={item.id} photo={item} />
