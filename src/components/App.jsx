dface76d266c5d3891981464c9b38a8c8d441e6a
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-09-25 22:49:12 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -29,6 +29,14 @@ export class App extends Component {
       this.setState({ dataPhoto: [] });
       this.fetchPhoto(searchTag, this.state.page);
     }
+
+    function smoothScrollGallery() {
+      window.scrollBy({
+        top: window.innerHeight * 0.8,
+        behavior: 'smooth',
+      });
+    }
+    smoothScrollGallery();
   }
 
   fetchPhoto = async (searchTag, page) => {
@@ -70,9 +78,12 @@ export class App extends Component {
   };
 
   handleFormSubmit = photoTag => {
-    console.log('submit');
     this.setState({ photoTag });
     this.setState({ page: 1 });
+    if (this.state.photoTag === photoTag) {
+      this.setState({ dataPhoto: [] });
+      this.fetchPhoto(photoTag, 1);
+    }
   };
 
   handleLoadMore = () => {
@@ -137,125 +148,3 @@ export class App extends Component {
     );
   }
 }
-// export class App extends Component {
-//   state = {
-//     dataPhoto: null,
-//     page: 1,
-//     showModal: false,
-//     photoTag: '',
-//     isLoading: false,
-//     currentLargeImageUrl: '',
-//     currentImageTags: '',
-//     currentHits: null,
-//     totalHits: null,
-//   };
-
-//   componentDidUpdate(_, prevState) {
-//     const searchTag = this.state.photoTag;
-//     if (prevState.photoTag !== searchTag) {
-//       this.setState({ dataPhoto: null });
-//       this.fetchPhoto(searchTag, this.state.page);
-//     }
-//   }
-
-//   fetchPhoto = async (searchTag, page) => {
-//     this.setState({ isLoading: true });
-//     try {
-//       const data = await getAllPhoto(searchTag, page);
-
-//       if (!this.state.dataPhoto) {
-//         this.setState({ dataPhoto: data.hits });
-//       } else {
-//         this.setState({ dataPhoto: [...this.state.dataPhoto, ...data.hits] });
-//       }
-
-//       this.setState({ currentHits: NUM_REQUESTED_PHOTOS * this.state.page });
-//       this.setState({ totalHits: data.totalHits });
-
-//       if (!data.hits.length) {
-//         this.notificationTry();
-//       }
-//     } catch (error) {
-//       this.notificationCatch(error.message);
-//     } finally {
-//       this.setState({ isLoading: false });
-//     }
-//   };
-
-//   openModal = e => {
-//     const currentLargeImageUrl = e.target.dataset.large;
-//     const currentImageTags = e.target.alt;
-
-//     this.setState({ currentLargeImageUrl, currentImageTags });
-//     this.toggleModal();
-//   };
-
-//   toggleModal = () => {
-//     this.setState(({ showModal }) => ({ showModal: !showModal }));
-//   };
-
-//   handleFormSubmit = photoTag => {
-//     this.setState({ photoTag });
-//     this.setState({ page: 1 });
-//   };
-
-//   handleLoadMore = () => {
-//     const page = this.state.page + 1;
-//     this.setState({ page: page });
-//     this.fetchPhoto(this.state.photoTag, page);
-//   };
-
-//   notificationTry() {
-//     Notiflix.Notify.warning(
-//       'Sorry, there are no images matching your search query. Please try again.',
-//       {
-//         position: 'center-center',
-//         fontSize: '18px',
-//         cssAnimationStyle: 'zoom',
-//         cssAnimationDuration: 1000,
-//         width: '380px',
-//       }
-//     );
-//   }
-
-//   notificationCatch(error) {
-//     Notiflix.Notify.warning(error, {
-//       position: 'center-center',
-//       fontSize: '16px',
-//       width: '340px',
-//     });
-//   }
-
-//   render() {
-//     const {
-//       showModal,
-//       dataPhoto,
-//       isLoading,
-//       currentLargeImageUrl,
-//       currentImageTags,
-//       currentHits,
-//       totalHits,
-//     } = this.state;
-//     const { handleFormSubmit, toggleModal, handleLoadMore, openModal } = this;
-
-//     return (
-//       <div>
-//         <GlobalStyle />
-//         <Searchbar onSubmit={handleFormSubmit} />
-//         {dataPhoto && <ImageGallery photos={dataPhoto} openModal={openModal} />}
-//         {isLoading && <Loader />}
-//         {showModal && (
-//           <Modal
-//             imageUrl={currentLargeImageUrl}
-//             imageTags={currentImageTags}
-//             onClose={toggleModal}
-//           />
-//         )}
-//         {dataPhoto && currentHits <= totalHits && (
-//           <Button handleLoadMore={handleLoadMore} />
-//         )}
-//         <ToastContainer autoClose={3000} />
-//       </div>
-//     );
-//   }
-// }
